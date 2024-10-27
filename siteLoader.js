075d180ccb0c50d772acd6e5c27d39f155ccb4cc
GrahamCoombs <151796539+GrahamCoombs@users.noreply.github.com>
2024-10-27 17:15:27 -0400

    trying some stuff out

---
+++ siteLoader.js
@@ -0,0 +1,8 @@
+// Fetch the JSON file containing the page information
+fetch('nav.json')
+.then(response => response.json)
+.then(nav => {
+    const navContainer = document.getElementById('nav-elements')
+    nav.forEach()
+
+})
